a7870e1c83bd6b4924626452718d67ecf4c36191
cairumeng <cairumeng@gmail.com>
2020-08-03 09:25:10 +0200

    initial commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,77 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+        <!-- Grid row -->
+        <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-6 mt-md-0 mt-3">
+
+                <!-- Content -->
+                <h5 class="text-uppercase">Laravel Weibo</h5>
+                <p>Here is a Twitter-like website created by a php framework,Laravel.</p>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none pb-3">
+
+            <!-- Grid column -->
+            <div class="col-md-3 mb-md-0 mb-3">
+
+                <!-- Links -->
+                <h5 class="text-uppercase">Weibo links</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Help</a>
+                    </li>
+                    <li>
+                        <a href="#!">About</a>
+                    </li>
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 mb-md-0 mb-3">
+
+                <!-- Links -->
+                <h5 class="text-uppercase">Laravel Links</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="https://laravel.com">Laravel</a>
+                    </li>
+                    <li>
+                        <a href="https://getbootstrap.com">Bootstrap</a>
+                    </li>
+                    <li>
+                        <a href="https://laracasts.com">Laracasts</a>
+                    </li>
+                    <li>
+                        <a href="https://larajobs.com">Job</a>
+                    </li>
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://laravel.com">Weibo-Rumeng.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    @include('layouts._header')
+    @yield('content')
+    @include('layouts._footer')
+
+</body>
+
+</html>
