2f1886e1b71c7cb034c3f2aab377cbbb85833203
cairumeng <cairumeng@gmail.com>
2020-08-08 13:38:49 +0200

    status store

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -12,6 +12,16 @@ class StatusesController extends Controller
     {
         $this->middleware('auth', ['except' => 'create']);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate(['contente' => 'required' | 'max:255']);
+        $user = Auth::user();
+        Status::create(['user_id' => $user->id, 'content' => $request->content]);
+        session()->flash('success', 'You have posted a new blog!');
+        return redirect()->back();
+    }
+
     public function destroy(Status $status)
     {
         $this->authorize('destroy', $status);
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
 @section ('content')
-<h4>This is the home page</h4>
+<div class="row">
+    <form method="POST" action="{{ route('statuses.store') }}" class="col-md-6">
+        @csrf
+        <div class="form-group">
+            <textarea name="content" class="form-control " rows="5" placeholder="post a new blog here..."></textarea>
+            <button class=" btn btn-lg btn-success float-right mt-3"> Post</button>
+        </div>
+    </form>
+    <div class="col-md-6">
+        @include('shared._user_info',['user'=>Auth::user()])
+    </div>
+</div>
+<hr>
 @stop
